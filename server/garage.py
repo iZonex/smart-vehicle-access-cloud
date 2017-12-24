5686348f859403be3d60a895d3cb26eca62b04e2
Dmytro Chystiakov <dlchistyakov@gmail.com>
2017-12-24 20:46:36 +0200

    [garage] add GarageAdmin, GarageVehicles

--- server/garage.py
+++ server/garage.py
@@ -1,12 +1,44 @@
 
+
+class GarageAdmins:
+
+    def __init__(self, garage_id):
+        pass
+
+    def remove(self, user_id):
+        pass
+
+    def add(self, user_id):
+        pass
+
+    def all(self):
+        pass
+
+
+class GarageVehicles:
+
+    def __init__(self, garage_id):
+        pass
+
+    def remove(self, user_id):
+        pass
+
+    def add(self, user_id):
+        pass
+
+    def all(self):
+        pass
+
+
 class Garage:
 
     def __init__(self, garage_id, owner, admins, vehicles):
         self.garage_id = garage_id
         self.owner = owner
-        self.admins = admins
-        self.vehicles = vehicles
+        self.admins = GarageAdmins(garage_id)
+        self.vehicles = GarageVehicles(garage_id)
 
+    @classmethod
     def get_garage(cls, garage_id):
         pass
 
