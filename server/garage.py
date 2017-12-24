96286d9749b55c31e821fbce11ca38503b398c79
Dmytro Chystiakov <dlchistyakov@gmail.com>
2017-12-24 20:47:41 +0200

    [garage] remove Garage methods

--- server/garage.py
+++ server/garage.py
@@ -41,21 +41,3 @@ class Garage:
     @classmethod
     def get_garage(cls, garage_id):
         pass
-
-    def get_admins(self):
-        pass
-
-    def add_admins(self, user):
-        pass
-
-    def remove_admins(self, user):
-        pass
-
-    def add_vehicles(self, vehicle):
-        pass
-
-    def remove_vehicles(self, vehicle):
-        pass
-
-    def get_vehicles(self):
-        pass
