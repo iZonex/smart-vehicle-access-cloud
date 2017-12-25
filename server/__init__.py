96cc9c36a05ee03ec142695db2c7b642eb51ee13
Dmytro Chystiakov <dlchistyakov@gmail.com>
2017-12-25 13:48:06 +0200

    [main] add import to __init__.py

---
+++ server/__init__.py
@@ -0,0 +1,14 @@
+from .garage import (GarageAdmins, GarageVehicles, Garage)
+from .tokens import VehicleToken
+from .user_account import (User, UserProfile)
+from .vehicle import Vehicle
+
+__ALL__ = [
+    'GarageAdmins',
+    'GarageVehicles',
+    'Garage',
+    'User',
+    'UserProfile',
+    'Vehicle',
+    'VehicleToken'
+]
