300007829fecb471f158d69cd98ec61b323c6bc2
Dmytro Chystiakov <dlchistyakov@gmail.com>
2017-12-25 22:19:21 +0200

    [vehicle-key] add 1 more module

---
+++ server/keys/digital_key.py
@@ -0,0 +1,19 @@
+
+class VehicleKey:
+
+    def __init__(self, user_id, vehicle_id, expired):
+        self.user_id = user_id
+        self.vehicle_id = vehicle_id
+        self.expired = expired
+
+    def create_key(self):
+        pass
+
+    def revoke_key(self):
+        pass
+
+    def all_keys(self):
+        pass
+
+    def get_key(self):
+        pass
