8d815690d8b6f44d2d24bda527f044030189b282
Dmytro Chystiakov <dlchistyakov@gmail.com>
2017-12-25 13:39:01 +0200

    [user_account] split User to User and Profile

--- server/user_account.py
+++ server/user_account.py
@@ -1,18 +1,22 @@
 
-class User:
+class UserProfile:
 
-    def __init__(self, user_id, first_name, second_name, middle_name,
-                 full_name, email, country, city, address, zip,
-                 phone_number, password):
-        self.user_id = user_id
-        self.first_name = first_name
-        self.second_name = first_name
-        self.middle_name = middle_name
-        self.full_name = full_name
-        self.email = email
+    def __init__(self, user_id, country, city, address, zip,
+                 phone_number):
         self.country = country
         self.city = city
         self.address = address
         self.zip = zip
         self.phone_number = phone_number
+
+class User:
+
+    def __init__(self, user_id, first_name, second_name, middle_name,
+                 full_name, email, password):
+        self.user_id = user_id
+        self.first_name = first_name
+        self.second_name = second_name
+        self.middle_name = middle_name
+        self.full_name = full_name
+        self.email = email
         self.password = password
